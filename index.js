64b5d15b61936846d47ef89fd897e48125753f8b
Casey Hareson <93360771+CaseyHareson@users.noreply.github.com>
2023-11-12 19:13:21 -0800

    New project objective and procedure updated to create a test for variables' value. Test created in index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 let admin, name;
+
 name = "John";
-admin = name; 
+
+admin = name;
+
 alert( admin );
+
+if (name==="John" && admin==="John") {
+    console.log("Test Succesful");
+} else if (name==="John" || admin==="John") {
+    console.log("Check your variables");
+} else {
+    console.log("Oops, something went wrong!")
+}
